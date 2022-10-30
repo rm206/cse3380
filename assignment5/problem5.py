e5a67a111603ada0e4be7e6ef05707fd66aa4ad4
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2022-10-30 03:49:05 -0500

    5.5

---
+++ assignment5/problem5.py
@@ -0,0 +1,18 @@
+from lib2to3.pgen2.pgen import generate_grammar
+import numpy as np
+from scipy.spatial import distance
+import matplotlib.pyplot as plt
+
+def generateCosineSimilarityScoreMatrix(vectors : np.array):
+    cosineSimilarityScoreMatrix = np.empty((vectors.shape[0], vectors.shape[0]))
+    for i in range(vectors.shape[0]):
+        for j in range(vectors.shape[0]):
+            cosineSimilarityScoreMatrix[i][j] = distance.cosine(vectors[i], vectors[j])*(-1) + 1
+    
+    return cosineSimilarityScoreMatrix
+
+generatedArray = generateCosineSimilarityScoreMatrix(np.random.rand(10,4))
+
+plt.matshow(generatedArray)
+plt.set_cmap('bwr')
+plt.show()
