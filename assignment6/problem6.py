accf19fbcb92f0827c60e8c332edf17541ed60ec
Rishabh Mediratta <rishabh@Rishabhs-MBP-2.dhost.uta.edu>
2022-11-21 12:34:52 -0600

    6.6

--- assignment6/problem6.py
+++ assignment6/problem6.py
@@ -8,7 +8,12 @@ b = dataset[:, 1]
 A = np.vstack([a, np.ones(len(a))]).T
 m, c = np.linalg.lstsq(A, b, rcond=None)[0]
 
+xn = a
+popt = np.polyfit(a, b, 2)
+yn = np.polyval(popt, xn)
+
 plt.plot(a, b, 'o', label='Original data', markersize=2)
-plt.plot(a, m*a + c, 'r', label='Fitted line')
+plt.plot(a, m*a + c, 'red', label="Linear fit")
+plt.plot(xn, yn, 'black', label = "Polynomial fit")
 plt.legend()
 plt.show()
